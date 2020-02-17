fb1d04b6b1b40f9b8aa19355b9c2dda37e5164ab
qq <13186061107@163.com>
2020-02-17 14:15:16 +0800

    角色继承

--- security-db/src/main/java/com/example/securitydb/config/SecurityConfig.java
+++ security-db/src/main/java/com/example/securitydb/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import com.example.securitydb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,8 +26,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userService);
     }
 
+    /**
+     * 密码编辑器
+     * @return
+     */
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * 角色继承
+     * @return
+     */
+    RoleHierarchy roleHierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        String hierarchy = "ROLE_dba > ROLE_admin \n ROLE_admin > ROLE_user";
+        roleHierarchy.setHierarchy(hierarchy);
+        return  roleHierarchy;
     }
 }
